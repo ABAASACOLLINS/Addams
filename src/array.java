c59ce3ec1ee4219cd7f0af8332acc0b24117fd06
abaasaaddams <abaasacollins@gmail.com>
2024-11-24 10:18:45 +0300

    arrays and loops

---
+++ src/array.java
@@ -0,0 +1,18 @@
+public class array {
+    public static void main(String[]args){
+        String [] dogs={"pitbull","german shepherd","local"};
+        System.out.println(dogs[1]);
+        String[]cars=new String[3];
+        //here we can use user input to accesss it
+
+        cars[0]="fienta";
+        cars[1]="sienta";
+        cars[2]="rumion";
+        System.out.println(cars[1]);
+        System.out.println(cars[2]);
+        for (int i = 0; i < cars.length ; i++) {
+            System.out.println(cars[i]);
+        }
+
+    }
+}
---
+++ src/nestedloops.java
@@ -0,0 +1,18 @@
+public class nestedloops {
+    public static void main( String[]args){
+        int col=5;
+        int row=2;
+        String sym= "@";
+        for (int i = 0; i <= row ; i++) {
+            System.out.println();
+            for (int z=0; z<=col; z++){
+                System.out.print(sym);
+            }
+
+        }
+ /*nested loops first execute the code inside then they finish it and execute the code outside ie they first finish the code inside then they go outside
+till the whole code is executed
+
+  */
+    }
+}
